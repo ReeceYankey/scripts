c2499caa580e6aa7b1f7f31254f8b6ea73be0b4c
reeceyankey <reeceyankey@gmail.com>
2026-02-13 22:12:13 -0500

    fix backup logic

--- bootstrap-dotfiles.sh
+++ bootstrap-dotfiles.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export BACKUP_DIR="$HOME/.config-backup"
+
 if [ ! -d "$HOME/dotfiles" ]; then
   git clone --bare git@github.com:ReeceYankey/dotfiles.git "$HOME/dotfiles"
 fi
@@ -12,11 +14,17 @@ function config {
 config config status.showUntrackedFiles no
 
 if config checkout; then
-  echo "Checked out config.";
+  echo "Checked out config."
 else
-  echo "Backing up pre-existing dot files to .config-backup";
-  mkdir -p .config-backup
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mv {} .config-backup/{}
+  echo "Backing up pre-existing dot files to .config-backup"
+  config ls-tree --full-tree -r --name-only HEAD | xargs -I {} sh -c ' 
+  if [ -e "$HOME/{}" ]; then 
+    mkdir -p "$BACKUP_DIR/$(dirname "{}")"
+    mv "$HOME/{}" "$BACKUP_DIR/{}" 
+    echo "moved $HOME/{} -> $BACKUP_DIR/{}"
+  fi
+  '
   config checkout
+  echo "Checked out config."
 fi;
 
